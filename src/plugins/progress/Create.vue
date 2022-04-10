33134ff2d01a4d6de0de40a1fac149223a403084
Nick <Deadeye07>
2022-04-09 23:02:34 -0400

    initial proposal input page

---
+++ src/plugins/progress/Create.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineProps({
+  space: Object,
+  preview: Boolean,
+  modelValue: Object
+});
+
+const emit = defineEmits(['update']);
+const update = form => {
+  emit('update', { key: 'progress', form });
+};
+</script>
+
+<template>
+  <ProgressConfig> TEST</ProgressConfig>
+</template>
--- src/plugins/progress/index.ts
+++ src/plugins/progress/index.ts
@@ -2,8 +2,4 @@ export default class Plugin {
   public author = 'nick';
   public version = '0.1.0';
   public name = 'progess';
-
-  incrementStep() {
-    
-  }
 }
